b8a19d3c6e9c7b279d0ee93dbc71a987e9441967
edwin  cabrejos gil <ecabrejos@developer.com>
2019-04-24 10:31:53 -0500

    final

--- app/src/main/java/com/demo/demosomosoh/MainActivity.java
+++ app/src/main/java/com/demo/demosomosoh/MainActivity.java
@@ -3,20 +3,49 @@ package com.demo.demosomosoh;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText tvEmail, tvLastName, tvAge, tvBirthday;
+    private Button btSave;
+    private FirebaseDatabase database;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        database = FirebaseDatabase.getInstance();
         setContentView(R.layout.activity_main);
         tvEmail = findViewById(R.id.tvEmail);
         tvLastName = findViewById(R.id.tvLastName);
         tvAge = findViewById(R.id.tvAge);
+        btSave = findViewById(R.id.btSave);
         tvBirthday = findViewById(R.id.tvBirthday);
+
+        btSave.setOnClickListener(v -> {
+
+
+            DatabaseReference myRef1 = database.getReference("correo");
+            myRef1.setValue(tvEmail.getText().toString());
+
+            DatabaseReference myRef2 = database.getReference("nombres");
+            myRef2.setValue(tvLastName.getText().toString());
+
+            DatabaseReference myRef3 = database.getReference("edad");
+            myRef3.setValue(tvAge.getText().toString());
+
+            DatabaseReference myRef4 = database.getReference("cumpleanios");
+            myRef4.setValue(tvBirthday.getText().toString());
+
+
+        });
     }
 
 
